334882866d233025a0d19ba29c367ab86de2f6c6
cammygames <multicraftserver@ymail.com>
2015-04-27 22:27:31 +0100

    Crime Span made need redoing got no data to test with tho

--- views/editWanted.php
+++ views/editWanted.php
@@ -1,131 +1,246 @@
 <?php
 
-// create a database connection, using the constants from config/config.php (which we loaded in index.php)
-$db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if (isset($_GET["ID"])) {
-    $wantedID = $_GET["ID"];
-	
-	if (isset($_POST["wantedID"]))
-	{
-		//vehOwner vehClass vehSide vehType vehPlate vehAlive vehAct vehCol vehInv
-		$wantedID   = $_POST["wantedID"];
-		$wantedName = $_POST["wantedName"];
-		$wantedCrimes = $_POST["wantedCrimes"];
-		$wantedBounty = $_POST["wantedBounty"];
-		$active = $_POST["active"];
-	
-		if (!$db_connection->connect_errno) 
-		{
-			if (isset($_POST['drop'])) 
-			{
-				$sql = "DELETE FROM `wanted` WHERE `wanted`.`wantedID` = '".$wantedID."'";
-			} 
-			else 
-			{
-				$sql = "UPDATE `wanted` SET `active` = '".$active."', `wantedName` = '".$wantedName."', `wantedBounty` = '".$wantedBounty."', `wantedCrimes` = '".$wantedCrimes."' WHERE `wanted`.`wantedID` = '".$wantedID."'";
-			}		
-
-			$result_of_query = $db_connection->query($sql);
-		}
-	}	
-} else {
-    echo "<center><h1 style='color:red'>" . $lang['idNotSet'] . "</h1></center>";
+function updated()
+{
+    echo "<div class='row'><div class='col-lg-12'>";
+    echo "<div class='alert alert-danger alert-dismissable'>";
+    echo "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
+    echo "<i class='fa fa-info-circle'></i> File updated</div></div></div>";//todo: use $lang['updated']
 }
 
-// change character set to utf8 and check it
+function stripArray($input)
+{
+    $array = array();
+    $array = explode("],[", $input);
+    $array = str_replace('"[[', "", $array);
+    $array = str_replace(']]"', "", $array);
+    $array = str_replace('`', "", $array);
+    return $array;
+}
+
+//Thanks PHP Doc's For these two Function (User: biohazard dot ge at gmail dot com ¶ )
+function before($this, $inthat)
+{
+    return substr($inthat, 0, strpos($inthat, $this));
+};
+
+function after($this, $inthat)
+{
+    if (!is_bool(strpos($inthat, $this)))
+        return substr($inthat, strpos($inthat, $this) + strlen($this));
+};
+
+$db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$db_connection->set_charset("utf8")) {
     $db_connection->errors[] = $db_connection->error;
 }
-?>
-<!-- Page Heading -->
-<div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">
-            <?php echo $lang['wanted']; ?>
-            <small><?php echo " " . $lang['editing']; ?></small>
-        </h1>
-        <ol class="breadcrumb">
-            <li class="active">
-                <i class="fa fa-wrench"></i><?php echo " " . $lang['wanted']; ?>
-            </li>
-        </ol>
-    </div>
-</div>
-<!-- /.row -->
-<div class="col-md-4"></div>
-<div class="col-md-4">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><i class="fa fa-child fa-fw"></i> <?php echo " " . $lang['wanted']; ?></h3>
-        </div>
-        <div class="panel-body">
-            <form method="post" action="editWanted.php?ID=<?php echo $wantedID; ?>" name="editform">
-                <?php
-                if (!$db_connection->connect_errno)
-                {
-                $sql = 'SELECT * FROM `wanted` WHERE `wantedID` ="' . $wantedID . '";';
-                $result_of_query = $db_connection->query($sql);
-                if ($result_of_query->num_rows > 0)
-                {
-                while ($row = mysqli_fetch_assoc($result_of_query))
-                {
-                echo "<center>";
-                echo "<h4>" . $lang['wID'] . ": <input id='wantedID' name='wantedID' type='text' value='" . $row["wantedID"] . "'></td><br/>";
-                echo "<h4>" . $lang['name'] . ":   <input id='wantedName' name='wantedName' type='text' value='" . $row["wantedName"] . "'></td><br/>";
-                echo "<h4>" . $lang['bounty'] . ":    <input id='wantedBounty' name='wantedBounty' type='text' value='" . $row["wantedBounty"] . "'></td><br/>";
-                echo "<h4>" . $lang['active'] . ":";
-                echo "<select id='active' name='active'>";
-                echo '<option value="0"';
-                if ($row['active'] == 0) {
-                    echo ' selected';
-                }
-                echo '>0</option>';
-                echo '<option value="1"';
-                if ($row['active'] == 1) {
-                    echo ' selected';
-                }
-                echo '>1</option>';
-                echo "</select>";
-                echo "</center>";
-                ?>
-        </div>
-    </div>
-</div>
-    <div class='col-lg-12'>
-        <div class='panel panel-default'>
-            <div class='panel-heading'>
-                <h3 class='panel-title'><i class='fa fa-suitcase  fa-fw'></i><?php echo " " . $lang['crimes'];?></h3>
+
+if (isset($_GET["ID"])) {
+    $wantedID = $_GET["ID"];
+
+    if (isset($_POST["editType"])) {
+        if (!$db_connection->connect_errno) {
+            switch ($_POST["editType"]) 
+			{
+                case "crimes":
+                    $crimes_value = $_POST["wantedCrimes"];
+					$update = "UPDATE `wanted` SET  `wantedCrimes` = '".$crimes_value."' WHERE `wanted`.`wantedID` = '".$wantedID."'";
+                    $result_of_query = $db_connection->query($update);
+                    updated();
+                    break;
+                case "wanted_info":                   
+					case 3:
+						$wantedName = $_POST["wantedName"];
+						$wantedBounty = intval($_POST["wantedBounty"]);
+						$active = intval($_POST["active"]);
+						$update = "UPDATE `wanted` SET `active` = '".$active."', `wantedName` = '".$wantedName."', `wantedBounty` = '".$wantedBounty."' WHERE `wanted`.`wantedID` = '".$wantedID."'";
+						$result_of_query = $db_connection->query($update);
+						updated();
+						break;
+            }
+
+        } else {
+            $this->errors[] = "Database connection problem.";
+        }
+    }
+
+    ?>
+    <!-- /.row -->
+    <?php
+    $sql = 'SELECT * FROM `wanted` WHERE `wantedID` ="' . $wantedID . '";';
+    $result_of_query = $db_connection->query($sql);
+    if ($result_of_query->num_rows > 0) {
+        while ($row = mysqli_fetch_assoc($result_of_query)) {
+            ?>
+            <div class="col-md-3" style="float:left;  padding-top:20px;">
+            <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="panel-title"><i class="fa fa-child fa-fw"></i><?php echo $row["wantedName"]; ?></h2>
             </div>
             <div class="panel-body">
-                <div class="col-md-4" style="padding-left:425px;">
+
+            <center><img src="assets/img/uniform/Default.jpg"/>
+            <?php
+				echo "<h4>" . $lang['name'] . ": " . $row["wantedName"] . "</h4>";
+            ?>
+
+            <span style="padding-left:15px;" class="fa fa-2x fa-bank">
+            <h4> <?php echo $lang['bounty'] . ": " . $row["wantedBounty"]; ?> </h4>
+            </span>
+            <?php
+            if ($row["active"] == 0) {
+                echo "<h4><span class='label label-success'>" . $lang["not"] . " " . $lang["active"] . "</span> ";
+            } else {
+                echo "<h4><span class='label label-danger'>" . $lang["active"] . "</span> ";
+            }
+
+            if ($_SESSION['user_level'] >= P_EDIT_PLAYER) {
+                echo '<a data-toggle="modal" href="#edit_wanted" class="btn btn-primary btn-xs" style="float: right;">';
+                echo '<i class="fa fa-pencil"></i>';
+                echo '</a>';
+            }
+        }
+    } else echo "<h1>" . $lang['noPlayer'] . "<h1>";
+    echo "</center>";
+    ?>
+    </div>
+    </div>
+    </div>
+
+    <div class="col-md-9" style="float:right; padding-top:20px;">
+        <div class="panel panel-default" style="float:left; width:100%; margin:0 auto;">
+            <ul id="myTab" class="nav nav-tabs">
+                <?php 
+				if ($_SESSION['user_level'] >= P_VIEW_WANTED) echo'<li><a href="#crimes" data-toggle="tab">'. $lang['crimes'] .'</a></li>';
+                ?>
+            </ul>
+            <div id="myTabContent" class="tab-content">
+                <div class="tab-pane fade in active well" id="crimes">
+                    <h4 style="centred"><?php echo $lang['crimes']; ?> </h4>
                     <?php
-                    echo "<textarea id='wantedCrimes' name='wantedCrimes' cols='100' rows='5'>" . $row["wantedCrimes"] . "</textarea>";
+                    $sql = 'SELECT * FROM `wanted` WHERE `wantedID` ="' . $wantedID . '";';
+                    $result_of_query = $db_connection->query($sql);
+                    while ($row = mysqli_fetch_assoc($result_of_query)) {
+                        if ($row["wantedCrimes"] !== '"[]"') {
+                            $return = stripArray($row["wantedCrimes"]);
+
+                            foreach ($return as $value) 
+							{
+								$name = before(',', $value);
+								$count = after(',', $value);
+								echo "<span class='label label-default' style='margin-right:3px; line-height:2;'>" . $name." : ". $count . "</span> ";
+                            }
+                        } else {
+                            echo $lang['no'] . " " . $lang['crimes'];
+
+                        }
+                    }
+					if ($_SESSION['user_level'] >= P_EDIT_WANTED)
+					{
                     ?>
-                </div>
+						<a data-toggle="modal" href="#edit_crimes" class="btn btn-primary btn-xs"
+						   style="float: right;">
+							<i class="fa fa-pencil"></i>
+						</a>
+					<?php   } ?>
+                </div> 
             </div>
         </div>
     </div>
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
-        <center>
-            <?php
-            echo "<input class='btn btn-lg btn-primary'  type='submit'  name='update' value='" . $lang['subChanges'] . "'>  ";
-            echo "<input class='btn btn-lg btn-danger'  type='submit'  name='drop' value='" . $lang['DELETE'] . "'>";
-            ?>
-            <br/>
-        </center>
+
+    <div class="modal fade" id="edit_crimes" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">
+					<span class="glyphicon glyphicon-pencil"></span><?php echo " " . $lang['edit'] . " " . $lang['crimes'];?>
+                    </h4>
+                </div>
+                <?php
+                $sql = 'SELECT * FROM `wanted` WHERE `wantedID` ="' . $wantedID . '";';
+                $result_of_query = $db_connection->query($sql);
+                while ($row = mysqli_fetch_assoc($result_of_query)) {
+                    ?>
+                    <form method="post" action="editWanted.php?ID=<?php echo $row['uid']; ?>" role="form">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input type="hidden" name="editType" value="crimes"/>
+
+                                <div class="row">
+                                    <textarea class="form-control" rows="10" name="wantedCrimes"><?php echo $row['wantedCrimes']; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-default" data-dismiss="modal" type="reset">Close</button>
+                            <button class="btn btn-primary" type="submit"><?php echo $lang['subChange']; ?></button>
+                        </div>
+                    </form>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="edit_wanted" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><span
+                            class="glyphicon glyphicon-pencil"></span><?php echo " " . $lang['edit'] . " " . $lang['player']; ?>
+                    </h4>
+                </div>
+                <?php
+                $sql = 'SELECT * FROM `wanted` WHERE `wantedID` ="' . $wantedID . '";';
+                $result_of_query = $db_connection->query($sql);
+                while ($row = mysqli_fetch_assoc($result_of_query)) {
+                    ?>
+
+                    <form method="post" action="editWanted.php?ID=<?php echo $row['uid']; ?>" role="form">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input type="hidden" name="editType" value="player_edit"/>
+
+                                <div class="row">
+                                    <center>
+                                        <?php
+											echo "<center>";
+											echo "<h4>" . $lang['name'] . ":   <input id='wantedName' name='wantedName' type='text' value='" . $row["wantedName"] . "'></td><br/>";
+											echo "<h4>" . $lang['bounty'] . ":    <input id='wantedBounty' name='wantedBounty' type='text' value='" . $row["wantedBounty"] . "'></td><br/>";
+											echo "<h4>" . $lang['active'] . ":";
+											echo "<select id='active' name='active'>";
+											echo '<option value="0"';
+											if ($row['active'] == 0) {
+												echo ' selected';
+											}
+											echo '>0</option>';
+											echo '<option value="1"';
+											if ($row['active'] == 1) {
+												echo ' selected';
+											}
+											echo '>1</option>';
+											echo "</select>";
+											echo "</center>";
+                                        ?>
+                                    </center>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-default" data-dismiss="modal" type="reset">Close</button>
+                            <button class="btn btn-primary" type="submit"><?php echo $lang['subChange']; ?></button>
+                        </div>
+                    </form>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
     </div>
 <?php
-};
+} else {
+    include("views/Errors/noID.php");
 }
-else {
-    echo "<center><h1 style='color:red'>ERROR NO RESULTS</h1></center>";
-}
-
-}
-else {
-    $this->errors[] = "Database connection problem.";
-}
-?>
-</form>
