f61e95f768aef9e8b5f1ebc74b5e6bf82e1ea252
Ppgtjmad <killer2zouk_11@hotmail.fr>
2015-05-14 18:32:58 +0200

    Create lang.fr.php
    French Translation

---
+++ config/lang/lang.fr.php
@@ -0,0 +1,155 @@
+<?php
+$lang = array();
+/*
+------------------------------------
+	Common Items / Header Items
+------------------------------------
+*/
+$lang['welcome'] = 'Bienvenue';
+$lang['players'] = 'Joueurs';
+$lang['player'] = 'Joueur';
+$lang['vehicles'] = 'Véhicules';
+$lang['vehicle'] = 'Véhicule';
+$lang['houses'] = 'Propriétés';
+$lang['house'] = 'Propriété';
+$lang['gangs'] = 'Gangs';
+$lang['gang'] = 'Gang';
+$lang['wanted'] = 'Recherché';
+$lang['staff'] = 'Equipe';
+$lang['search'] = 'Chercher';
+$lang['edit'] = 'Editer';
+$lang['editing'] = 'Edition';
+$lang['editor'] = 'Editeur';
+$lang['members'] = 'Membres';
+$lang['subChange'] = 'Valider Changements';
+$lang['editPlayer'] = 'Editer Joueur';
+$lang['DELETE'] = 'Supprimer';
+$lang['quickLook'] = 'Vue Rapide';
+$lang['PID'] = 'PID';
+$lang['curPlayers'] = 'Joueurs Actuels';
+$lang['statOver'] = 'Vue Statistiques';
+$lang['overview'] = 'Vue d\'Ensemble';
+$lang['viewAll'] = 'Voir Tout';
+$lang['subChanges'] = 'Mise à Jour';
+$lang['yes'] = 'Oui';
+$lang['no'] = 'Non';
+$lang['car'] = 'Terrestre';
+$lang['air'] = 'Aérien';
+$lang['ban'] = 'Ban';
+/*
+-----------------
+	Errors
+-----------------
+*/
+$lang['error'] = 'Erreur';
+$lang['dbError'] = 'Problème de Connection à la BDD';
+$lang['idNotSet'] = 'ID NON DEFINIE';
+$lang['noRes'] = 'ERREUR AUCUN RESULTAT';
+$lang['noMessage'] = 'Aucun message';
+$lang['topRich'] = 'Top 10 Joueurs les plus riches';
+$lang['noHouse'] = 'Aucune Propriété';
+$lang['noCar'] = 'Aucun Véhicule';
+$lang['noPerm'] = 'Vous n\'avez pas la permission de voir cette page';
+/*
+---------------
+	Ranks
+---------------
+*/
+$lang['support'] = 'Support';
+$lang['mod'] = 'Modérateur';
+$lang['administrator'] = 'Administrateur';
+/*
+------------------
+	Menu Items
+------------------
+*/
+$lang['navProfile'] = 'Profil';
+$lang['navAdmin'] = 'Admin';
+$lang['navNewUser'] = 'Ajouter un utilisateur';
+$lang['navDashboard'] = 'Tableau de bord';
+$lang['navLogOut'] = 'Déconnection';
+$lang['first'] = 'Premier';
+$lang['previous'] = 'Précédent';
+$lang['next'] = 'Suivant';
+$lang['last'] = 'Dernier';
+/*
+-------------------------------
+	Table Headers / Buttons 
+-------------------------------
+*/
+$lang['id'] = 'ID';
+$lang['uid'] = 'ID Utilisateur';
+$lang['staffName'] = 'Nom Equipe';
+$lang['emailAdd'] = 'Email';
+$lang['rank'] = 'Niveau';
+$lang['name'] = 'Nom';
+$lang['wantList'] = 'Interpol';
+$lang['aliases'] = 'Alias';
+$lang['owner'] = 'Propriétaire';
+$lang['maxMembers'] = 'Membres Max';
+$lang['members'] = 'Membres';
+$lang['bank'] = 'Banque';
+$lang['cash'] = 'Argent';
+$lang['cop'] = 'Forces de l\'ordre';
+$lang['police'] = 'Forces de l\'ordre';
+$lang['medic'] = 'Médecin';
+$lang['medics'] = 'Médecins';
+$lang['civil'] = 'Civils';
+$lang['civ'] = 'Civ';
+$lang['side'] = 'Camp';
+$lang['admin'] = 'Admin';
+$lang['donator'] = 'Donateur';
+$lang['licenses'] = 'Compétences';
+$lang['gear'] = 'Equipement';
+$lang['active'] = 'Actif';
+$lang['playerID'] = 'ID Joueur';
+$lang['wID'] = 'ID Interpol';
+$lang['bounty'] = 'Mise à prix';
+$lang['crimes'] = 'Délits';
+$lang['GUID'] = 'GUID';
+$lang['position'] = 'Position';
+$lang['inventory'] = 'Inventaire';
+$lang['containers'] = 'Stockage';
+$lang['class'] = 'Classe';
+$lang['type'] = 'Type';
+$lang['plate'] = 'Immatriculation';
+$lang['alive'] = 'Etat';
+$lang['colour'] = 'Couleur';
+/*
+---------------------------
+	SQL Phone Module
+---------------------------
+*/
+$lang['from'] = 'De';
+$lang['to'] = 'A';
+$lang['message'] = 'Message';
+$lang['time'] = 'Heure';
+/*
+--------------------------
+	Need translation
+--------------------------
+*/
+$lang['setup'] = 'La BDD a été configuré';
+$lang['upgrade'] = 'La BDD a été mise à niveau';
+$lang['Kick'] = 'Ejecter';
+$lang['noPlayer'] = 'Le joueur est introuvable';
+$lang['enterPass'] = 'Tapez un nouveau mot de passe';
+$lang['password'] = 'Mot de Passe';
+$lang['repeat'] = 'Répéter';
+$lang['login'] = 'Identification';
+$lang['cancel'] = 'Annuler';
+$lang['picture'] = 'Image';
+$lang['changePass'] = 'Changer Mot de Passe';
+$lang['gameServers'] = 'Serveurs';
+$lang['page'] = 'Page';
+$lang['not'] = 'N\'est pas';
+$lang['blacklisted'] = 'Blacklisté';
+$lang['arrested'] = 'Arrêté';
+$lang['finances'] = 'Finances';
+$lang['ship'] = 'Bateau';
+$lang['store'] = 'Déposer';
+$lang['add'] = 'Ajouter';
+$lang['new'] = 'Nouveau';
+$lang['note'] = 'Note';
+$lang['uid'] = 'UID';
+?>
